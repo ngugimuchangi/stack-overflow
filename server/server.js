784446e2f4a765dc3ddd244f9da8ddff5c1124dc
ngugimuchangi <tech.curious14@gmail.com>
2023-11-29 10:25:37 +0300

    Backend server set up

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./utils/db');
+const router = require('./routes');
+const errorHandler = require('./middlewares/error');
+const routerNotFound = require('./middlewares/404');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+let server;
+
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+app.use(routerNotFound);
+app.use(errorHandler);
+
+// Start server only when database is connected
+const intervalId = setInterval(() => {
+  if (db.isAlive) {
+    clearInterval(intervalId);
+    server = app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }
+}, 1000);
+
+// Graceful shutdown
+process.on('SIGINT', async () => {
+  await db.disconnect();
+  server.close(() => {
+    console.log('Server closed. Database instance disconnected.');
+  });
+});
