4c60b4a1522e366d90d1868f034d0341a105ae13
ngugimuchangi <tech.curious14@gmail.com>
2023-11-29 10:23:15 +0300

    Shared modules across the project

---
+++ server/common/constants.js
@@ -0,0 +1,10 @@
+const constants = {
+  HTTP_OK: 200,
+  HTTP_CREATED: 201,
+  HTTP_BAD_REQUEST: 400,
+  HTTP_UNAUTHORIZED: 401,
+  HTTP_NOT_FOUND: 404,
+  HTTP_INTERNAL_SERVER_ERROR: 500,
+};
+
+module.exports = constants;
---
+++ server/common/types.js
@@ -0,0 +1,5 @@
+/**
+ * @typedef {import('express').Request} Request
+ * @typedef {import('express').Response} Response
+ * @typedef  {import('express').Next} Next Next function
+ */
