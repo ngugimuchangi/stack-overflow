b14edfeaa57be48d1c4fc9f3901fa3d2ff5bf472
Bezalell Barnet W <118984781+BezzB@users.noreply.github.com>
2023-12-01 01:30:16 +0300

    Update setup

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,5 +1,3 @@
-// Homepage.js
-
 import React from 'react';
 import Banner from './Banner';
 import Menu from './Menu';
@@ -7,24 +5,30 @@ import Question from './Question';
 
 const Homepage = () => {
   // Sample data, replace with actual data fetched from the server
-//   const questions = [...];
+  // const questions = [...];
 
   return (
     <div className="homepage">
       <Banner />
-       <div className="main-body">
+      <div className="main-body">
         <Menu activeLink="questions" />
         <div className="questions-section">
-          <h2>All Questions</h2>
-          <button>Ask Question</button>
-          {}
+          <div className="main-section">
+            <h2>All Questions</h2>
+            <button>Ask Question</button>
+          </div>
+          <div className="buttons">
+            <button>Newest</button>
+            <button>Active</button>
+            <button>Unanswered</button>
+          </div>
           {/* <div className="questions-list">
             {questions.map((question) => (
               <Question key={question.id} question={question} />
             ))}
           </div> */}
         </div>
-      </div> 
+      </div>
     </div>
   );
 };
