da3f80b725020a0b6373d19bc9d020acb1ac651b
ngugimuchangi <tech.curious14@gmail.com>
2023-11-30 03:58:50 +0300

    Addition of auto updated timestamps fields

--- server/models/comments.js
+++ server/models/comments.js
@@ -1,10 +1,14 @@
 const { Schema } = require('mongoose');
 
-const Comment = new Schema({
-  text: { type: String, required: true },
-  com_by: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  com_date_time: { type: Date, default: Date.now },
-  votes: { type: Number, default: 0 },
-});
+const Comment = new Schema(
+  {
+    text: { type: String, required: true },
+    com_by: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    votes: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = Comment;
--- server/models/questions.js
+++ server/models/questions.js
@@ -2,16 +2,19 @@ const { model, Schema } = require('mongoose');
 const Answer = require('./answers');
 
 // Question schema
-const Question = new Schema({
-  title: { type: String, required: true },
-  summary: { type: String, required: true },
-  text: { type: String, required: true },
-  tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
-  asked_by: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  question_date_time: { type: Date, default: Date.now },
-  answers: [Answer],
-  ask_date_time: { type: Date, default: Date.now },
-  views: { type: Number, default: 0 },
-});
+const Question = new Schema(
+  {
+    title: { type: String, required: true },
+    summary: { type: String, required: true },
+    text: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    answers: [Answer],
+    views: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model('Questions', Question);
